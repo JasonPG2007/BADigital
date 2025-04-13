39b8085ffd036eadbb548192a293285a8235ff3b
Nguyễn Quốc Bảo An <baoandng07@gmail.com>
2025-04-13 15:22:16 -0400

    Edit content in explore button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -50,7 +50,6 @@ export default function Header() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            {" "}
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
